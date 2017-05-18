7b5b67f7430c224a4f6a212107d3aca89b17955e
Sergey Shandar <sergey@live.com.au>
2017-05-18 14:11:08 -0700

    fixing dotnet for autorest

--- install.ps1
+++ install.ps1
@@ -24,9 +24,9 @@ git clone -q --branch=sergey-new-autorest-fixes-tests2 https://github.com/sergey
 .\lib\remove.ps1 -path $env:TEST_COMMON
 mkdir $env:TEST_COMMON
 
-"Installing .Net Core..."
-$dotnetUrl = "https://download.microsoft.com/download/E/7/8/E782433E-7737-4E6C-BFBF-290A0A81C3D7/dotnet-dev-win-x64.1.0.4.zip"
-.\lib\download-and-unzip.ps1 -url $dotnetUrl -zip "dotnet.zip" -dest $env:TEST_DOTNET_FOLDER
+"Installing .Net Core SDK..."
+$dotnetSdkUrl = "https://download.microsoft.com/download/E/7/8/E782433E-7737-4E6C-BFBF-290A0A81C3D7/dotnet-dev-win-x64.1.0.4.zip"
+.\lib\download-and-unzip.ps1 -url $dotneSdkUrl -zip "dotnetsdk.zip" -dest $env:TEST_DOTNET_FOLDER
 
 "Installing Node..."
 $nodeUrl = "https://nodejs.org/dist/" + $env:TEST_NODE_VER + "/" + $env:TEST_NODE + ".zip"
@@ -37,6 +37,12 @@ npm install -g --silent autorest
 
 "Updating AutoRest..."
 autorest --reset --feed=sergey-shandar
+
+"Fixing AutoRest..."
+$dnUrl = "https://download.microsoft.com/download/2/4/A/24A06858-E8AC-469B-8AE6-D0CEC9BA982A/dotnet-win-x64.1.0.5.zip"
+$dnOutput = "~\.autorest\frameworks"
+.\lib\remove.ps1 -path $dnOutput
+.\lib\download-and-unzip.ps1 -url $dnUrl -zip "dotnet.zip" -dest $dnOutput
 
 "Language = $env:TEST_LANG"
 "Project = $env:TEST_PROJECT"
