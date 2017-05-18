4a717e3cd90b99da50293cf2b4de12370356fec4
Sergey Shandar <sergey@live.com.au>
2017-05-17 21:33:54 -0700

    update env ver

--- common.ps1
+++ common.ps1
@@ -1,5 +1,7 @@
 $env:TEST_COMMON = Join-Path (pwd) "x"
 
+$env:TEST_PROJECT = $env:TEST_PROJECT.Replace('_', '\')
+
 $env:TEST_NODE_VER = "v7.10.0"
 $env:TEST_NODE = "node-" + $env:TEST_NODE_VER + "-win-x64"
 $env:TEST_NODE_FOLDER = Join-Path $env:TEST_COMMON $env:TEST_NODE
@@ -11,8 +13,6 @@ $env:Path = $env:TEST_DOTNET_FOLDER + ";" + $env:Path
 $testFolder = Join-Path (Join-Path (pwd) "_\src\SDKs") $env:TEST_PROJECT
 
 "TestFolder = $testFolder"
-
-$env:TEST_PROJECT = $env:TEST_PROJECT.Replace('_', '\')
 
 $env:TEST_PROJECT_FOLDER = (Get-ChildItem -Path $testFolder -Filter "generate.cmd" -Recurse).Directory.FullName
 "env:TEST_PROJECT_FOLDER = $env:TEST_PROJECT_FOLDER"
