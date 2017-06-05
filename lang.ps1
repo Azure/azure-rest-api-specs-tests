62eedf1b9f44a88dfa4d35712a343843f37eee36
Sergey Shandar <sergey@live.com.au>
2017-06-05 13:52:07 -0700

    using jsonrpcclient

--- lang.ps1
+++ lang.ps1
@@ -6,6 +6,8 @@ if ($env:TEST_LANG -eq "go")
     $scriptFile = ".\" + $env:TEST_LANG + "\" + $script + ".ps1"
     "Running $scriptFile..."
     & $scriptFile
+} elseif ($env:TEST_LANG -eq "cs") {
+    $env:CODEGEN = "Azure.JsonRpcClient"
 } else {
     $env:CODEGEN = "Azure.CSharp"    
 }
