71f08ddc80269156e4076fd50a326c8386a70eb8
Sergey Shandar <sergey@live.com.au>
2017-06-06 22:02:23 -0700

    fixing XML

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 param([string]$TEST_PROJECT)
 
+$current = (pwd)
+
 "Building..."
 
 if ($TEST_PROJECT) 
@@ -15,11 +17,36 @@ if (-Not $?)
 
 .\lang.ps1 -script "build"
 
+"Adding a project reference..."
+$xmlns = "http://schemas.microsoft.com/developer/msbuild/2003"
+$namespace = @{x=$xmlns}
+
+$xmlFile = Join-Path $current "_/src/SDKs/AzSdk.reference.props"
+
+[xml]$xml = Get-Content $xmlFile
+
+$projectReferenceList = Select-Xml -Xml $xml -XPath "//x:Project/x:ItemGroup/x:PackageReference[@Include='Microsoft.Rest.ClientRuntime.Test']" -Namespace $namespace
+$projectReference = $projectReferenceList.Node
+
+If (-Not $projectReference)
+{
+    $projectReference = $xml.CreateElement("PackageReference", $xmlns)
+    $projectReference.SetAttribute("Include", "Microsoft.Rest.ClientRuntime.Test")
+
+    $itemGroupList = Select-Xml -Xml $xml -XPath "//x:Project/x:ItemGroup[not(@*)][1]" -Namespace $namespace
+    $itemGroup = $itemGroupList.Node
+    $itemGroup.AppendChild($projectReference)
+}
+
+$projectReference.SetAttribute("Version", "1.0.200.170")
+
+$xml.Save($xmlFile)
+
 "Generating SDK..."
 autorest -Modeler $env:TEST_MODELER -CodeGenerator $env:CODEGEN -Namespace $env:TEST_PROJECT_NAMESPACE -Input $env:TEST_INPUT -Output $env:TEST_PROJECT_FOLDER
 
 "Restoring test project NuGet packages..."
-dotnet restore $env:TEST_PROJECT_TEST
+dotnet restore $env:TEST_PROJECT_TEST -s "https://ci.appveyor.com/nuget/rest-client-runtime-test-net-p-lft6230b45rt"
 
 dotnet build $env:TEST_PROJECT_TEST
 if (-Not $?)
