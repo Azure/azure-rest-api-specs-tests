d442a27980dfde0fd1d81e5cf01ef42d05db897b
Sergey Shandar <sergey@live.com.au>
2017-06-08 13:34:50 -0700

    Test extension

--- build.ps1
+++ build.ps1
@@ -43,7 +43,7 @@ If (-Not $projectReference)
     $itemGroup.AppendChild($projectReference)
 }
 
-$projectReference.SetAttribute("Version", "1.0.200.176")
+$projectReference.SetAttribute("Version", "1.0.200.180")
 
 $xml.Save($xmlFile)
 
