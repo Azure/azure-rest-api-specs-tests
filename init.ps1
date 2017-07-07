374d5c2eaa69eea096a76b8acc5bf969f81fbfa3
Sergey Shandar <sergey@live.com.au>
2017-07-06 18:08:15 -0700

    No cloning azure-rest-api-specs

--- init.ps1
+++ init.ps1
@@ -5,14 +5,6 @@ $branch = if ($branch) { $branch } else { "master" }
 
 Import-Module ".\lib.psm1"
 
-"Cloning azure-rest-api-specs..."
-Remove-All -path azure-rest-api-specs
-
-$testRep = "https://github.com/$fork/azure-rest-api-specs"
-"Azure REST API Specs repository: $testRep"
-"Azure REST API Specs branch: $branch"
-git clone -q --branch=$branch $testRep azure-rest-api-specs
-
 "Cloning azure-sdk-for-net..."
 # $sdkFork = "Azure"
 $sdkFork = "sergey-shandar"
